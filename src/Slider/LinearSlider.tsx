9fd3aecb93b85dee5c0acb70673d57ab732709e2
susonthapa <33973551+susonthapa@users.noreply.github.com>
2024-01-14 18:39:17 +0545

    added the parent slider spring animation

--- src/Slider/LinearSlider.tsx
+++ src/Slider/LinearSlider.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
-import { View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import Animated, { Easing, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withSpring, withTiming } from "react-native-reanimated";
+import Animated, { Easing, useAnimatedReaction, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withSpring, withTiming } from "react-native-reanimated";
 
 const WIDTH = 200
 const HEIGHT = 70
@@ -23,29 +22,35 @@ function getIdleAnimation() {
 
 
 const LinearSlider = () => {
-  const offset = useSharedValue(0)
+  const ovalOffset = useSharedValue(0)
+  const sliderOffset = useSharedValue(0)
+  const maxLimit = (WIDTH * (1 - 0.375) - 12)
   const ovalStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: offset.value }]
+      transform: [{ translateX: ovalOffset.value }]
+    }
+  })
+
+  const sliderStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: sliderOffset.value }]
     }
   })
 
   const pan = Gesture.Pan()
     .onChange((e) => {
-      let currentValue = offset.value
+      let currentValue = ovalOffset.value
 
-      const maxLimit = (WIDTH * (1 - 0.375) - 12)
       const updatedValue = currentValue + e.changeX
-      if ((e.changeX > 0 && offset.value < maxLimit)) {
+      if ((e.changeX > 0 && ovalOffset.value < maxLimit)) {
         currentValue = Math.min(maxLimit, updatedValue)
-      } else if (e.changeX < 0 && offset.value > 0) {
+      } else if (e.changeX < 0 && ovalOffset.value > 0) {
         currentValue = Math.max(0, currentValue + e.changeX)
       }
-      offset.value = currentValue
+      ovalOffset.value = currentValue
     })
     .onFinalize((e) => {
-      let currentValue = offset.value
-      const maxLimit = (WIDTH * (1 - 0.375) - 12)
+      let currentValue = ovalOffset.value
       const halfLimit = maxLimit / 2
       if (currentValue >= halfLimit) {
         currentValue = maxLimit
@@ -53,24 +58,38 @@ const LinearSlider = () => {
         currentValue = 0
       }
       if (currentValue === 0) {
-        offset.value = withSequence(
+        ovalOffset.value = withSequence(
           withTiming(0),
           withDelay(100,
             getIdleAnimation()
           )
         )
       } else {
-        offset.value = withTiming(currentValue)
+        ovalOffset.value = withTiming(currentValue, {
+          easing: Easing.exp
+        })
       }
     })
 
+  useAnimatedReaction(() => {
+    return ovalOffset.value
+  }, (current, previous) => {
+    if (current !== previous && current === maxLimit) {
+      sliderOffset.value = withSequence(
+        withTiming(15, { duration: 100 }),
+        withSpring(0, {
+          duration: 1000
+        })
+      )
+    }
+  })
 
   useEffect(() => {
-    offset.value = getIdleAnimation()
+    ovalOffset.value = getIdleAnimation()
   }, [])
 
   return (
-    <View style={{
+    <Animated.View style={[{
       width: WIDTH,
       height: HEIGHT,
       borderRadius: HEIGHT / 2,
@@ -78,7 +97,7 @@ const LinearSlider = () => {
       borderWidth: 2,
       justifyContent: 'center',
       padding: 4,
-    }}>
+    }, sliderStyle]}>
       <GestureDetector gesture={pan}>
         <Animated.View style={[{
           backgroundColor: '#333358',
@@ -88,8 +107,7 @@ const LinearSlider = () => {
         }, ovalStyle]}>
         </Animated.View>
       </GestureDetector>
-
-    </View>
+    </Animated.View>
   )
 }
 
